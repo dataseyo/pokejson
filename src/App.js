c9a578fcc620f1aae13a23d9282cb25f67f40ba6
dataseyo <zshifrel@asu.edu>
2022-01-31 10:24:51 -0500

    Shiny Poke Json first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react"
 
 function App() {
+  const [pokeData, setPokeData] = useState({});
+  const [pokenumber, setPokenumber] = useState(2);
+  const [sprite, setSprite] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
+
+  // update pokeData onrender, checking for pokenumber as a dependency
+  // to change the fetch request
+  useEffect(() => {
+    console.log("fetch");
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokenumber}`)
+      .then((res) => res.json())
+      .then((data) => setPokeData(data));
+  }, [pokenumber]);
+
+  // function that increments
+  const changePokemon = () => {
+    console.log({ pokenumber });
+    setPokenumber((prevNumber) => prevNumber + 1);
+    setSprite(pokeData.sprites.front_shiny)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shiny Poke JSON</h1>
+      <button onClick={changePokemon}>Change Pokemon</button>
+      <pre>{JSON.stringify(sprite, null, 1)}</pre>
+      <img src={`${sprite}`}></img>
     </div>
   );
 }
