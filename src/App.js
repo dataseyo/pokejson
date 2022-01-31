814744bf447e0b8f9ded62ddd1f7454104e1dfa9
dataseyo <zachary.shifrel@gmail.com>
2022-01-31 13:17:57 -0500

    Default pokemon and styling added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React, { useEffect, useState } from "react"
 function App() {
   const [pokeData, setPokeData] = useState({});
   const [pokenumber, setPokenumber] = useState(2);
-  const [sprite, setSprite] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
+  const [shinySprite, setShinySprite] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
+  const [normalSprite, setNormalSprite] = useState("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
 
   // update pokeData onrender, checking for pokenumber as a dependency
   // to change the fetch request
@@ -15,19 +16,36 @@ function App() {
       .then((data) => setPokeData(data));
   }, [pokenumber]);
 
-  // function that increments
+  // function that increments pokenumber to change displayed poke 
   const changePokemon = () => {
     console.log({ pokenumber });
     setPokenumber((prevNumber) => prevNumber + 1);
-    setSprite(pokeData.sprites.front_shiny)
+    setShinySprite(pokeData.sprites.front_shiny)
+    setNormalSprite(pokeData.sprites.front_default)
   };
 
   return (
     <div className="App">
       <h1>Shiny Poke JSON</h1>
-      <button onClick={changePokemon}>Change Pokemon</button>
-      <pre>{JSON.stringify(sprite, null, 1)}</pre>
-      <img src={`${sprite}`}></img>
+      <button 
+        onClick={changePokemon}
+        className="button"
+      >
+        Change Pokemon
+      </button>
+      <pre>{JSON.stringify(shinySprite, null, 1)}</pre>
+
+      <div className="poke-img-container">
+        <div className="shiny-img">
+          <p>Shiny</p>
+          <img src={`${shinySprite}`}></img>
+        </div>
+
+        <div className="normal-img">
+          <p>Default</p>
+          <img src={`${normalSprite}`}></img>
+        </div>
+      </div>
     </div>
   );
 }
